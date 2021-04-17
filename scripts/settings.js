eb0085a5e7d648200c43c34cad4c6872abae959c
M.C <hanhchaumn@gmail.com>
2021-04-17 21:40:17 +0100

    Add animation duration customization to info card

--- scripts/settings.js
+++ scripts/settings.js
@@ -1,56 +1,69 @@
 export const ModuleOptions = {
-    LITE_MODE: "litemode"
+	LITE_MODE: 'litemode',
+	ANIMATION_DURATION: 'animation-duration',
+	ANIMATION_DURATION_CHOICES: {
+		None: 'None',
+		Short: 'Short',
+		Medium: 'Medium',
+		Long: 'Long'
+	}
 };
 
 export class ModuleSettings {
-    static MODULE_NAME = 'bugsheet';
+	static MODULE_NAME = 'bugsheet';
 
 	static registerSettings() {
-        game.settings.register(this.MODULE_NAME, ModuleOptions.LITE_MODE, this._buildBooleanConfig(ModuleOptions.LITE_MODE));
+		game.settings.register(this.MODULE_NAME, ModuleOptions.LITE_MODE, this._buildBooleanConfig(ModuleOptions.LITE_MODE));
+		game.settings.register(
+			this.MODULE_NAME,
+			ModuleOptions.ANIMATION_DURATION,
+			this._buildChoiceConfig(ModuleOptions.ANIMATION_DURATION, ModuleOptions.ANIMATION_DURATION_CHOICES, 'MEDIUM')
+		);
 	}
 
 	static getSetting(option) {
 		return game.settings.get(this.MODULE_NAME, option);
-    }
+	}
 
-    /** @private */
-    static _getNameConfig(optionName) {
-        return {
-            name: `${this.MODULE_NAME}.${optionName}-s`,
-            hint: `${this.MODULE_NAME}.${optionName}-l`
-        };
-    }
+	/** @private */
+	static _getNameConfig(optionName) {
+		return {
+			name: `${this.MODULE_NAME}.${optionName}-s`,
+			hint: `${this.MODULE_NAME}.${optionName}-l`
+		};
+	}
 
-    /** @private */
-    static _buildChoiceConfig(optionName, choices, config = {}) {
-        const defaultConfig = {
-            scope: "client",
-            config: true,
-            default: false,
-            type: String,
-            choices,
-            onChange: x => window.location.reload()
-        };
-        return {
-            ...defaultConfig,
-            ...this._getNameConfig(optionName),
-            ...config
-        };
-    }    
-    
-    /** @private */
-    static _buildBooleanConfig(optionName, config = {}) {
-        const defaultConfig = {
-            scope: "client",
-            config: true,
-            default: false,
-            type: Boolean,
-            onChange: x => window.location.reload()
-        };
-        return {
-            ...defaultConfig,
-            ...this._getNameConfig(optionName),
-            ...config
-        };
-    }
+	/** @private */
+	static _buildChoiceConfig(optionName, choices, defaultOption, config = {}) {
+		console.warn(choices);
+		const defaultConfig = {
+			scope: 'client',
+			config: true,
+			default: defaultOption,
+			type: String,
+			choices,
+			onChange: (x) => window.location.reload()
+		};
+		return {
+			...defaultConfig,
+			...this._getNameConfig(optionName),
+			...config
+		};
+	}
+
+	/** @private */
+	static _buildBooleanConfig(optionName, config = {}) {
+		const defaultConfig = {
+			scope: 'client',
+			config: true,
+			default: false,
+			type: Boolean,
+			onChange: (x) => window.location.reload()
+		};
+		return {
+			...defaultConfig,
+			...this._getNameConfig(optionName),
+			...config
+		};
+	}
 }
--- scripts/sheet.js
+++ scripts/sheet.js
@@ -4,16 +4,33 @@ export default class SheetStyler {
 	static process(sheet, html, data) {
 		this.style(sheet, html, data);
 		this.addSummaryExpandableSection(sheet, html);
+		this.setInfoCardAnimationDuration(sheet, html);
 	}
 
 	static addSummaryExpandableSection(sheet, html) {
-		html.find('.item .item-detail:not(.item-charges)').click(event => sheet._onItemSummary(event));
+		html.find('.item .item-detail:not(.item-charges)').click((event) => sheet._onItemSummary(event));
 	}
 
 	static style(sheet, html, data) {
-		const isLiteMode = ModuleSettings.getSetting(ModuleOptions.LITE_MODE);
-		if (!isLiteMode) {
-			html.addClass("bugsheet-modified");
+		if (!ModuleSettings.getSetting(ModuleOptions.LITE_MODE)) {
+			html.addClass('bugsheet-modified');
 		}
+	}
+
+	static setInfoCardAnimationDuration(sheet, html, data) {
+		let duration = 0.2;
+		const durationChoice = ModuleSettings.getSetting(ModuleOptions.ANIMATION_DURATION);
+		switch (durationChoice) {
+			case ModuleOptions.ANIMATION_DURATION_CHOICES.Short:
+				duration = 0.1;
+				break;
+			case ModuleOptions.ANIMATION_DURATION_CHOICES.Long:
+				duration = 0.3;
+				break;
+			case ModuleOptions.ANIMATION_DURATION_CHOICES.None:
+				duration = 0;
+				break;
+		}
+		html.find('#item-info-container').css("transition", `width ${duration}s ease`)
 	}
 }
